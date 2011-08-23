6c2996779bec4c7b37e7efd5ce4b69dfd9d2b827
Antonio Roberto <forevertonny@gmail.com>
2011-08-23 20:56:11 -0300

    ajust-design

---
+++ jquery.Ring.js
@@ -0,0 +1,42 @@
+(function($){
+  $.fn.extend({
+    ring: function(options) {
+      var defaults = {
+        timeInterval: 5000,
+        position: 'relative',
+        loopInterval: true,
+        animateDistanceInPixels: 5,
+        animateVelocity: 'fast'
+      }
+
+      var options = $.extend(defaults,options)
+
+      return this.each(function(){
+        var o = options;
+        var obj = $(this);
+
+        var animate = {
+          run: function(object) {
+            object.animate({"left": "+="+o.animateDistanceInPixels+"px"}, o.animateVelocity).
+                    animate({"left": "-="+(o.animateDistanceInPixels*2)+"px"}, o.animateVelocity).
+                    animate({"left": "+="+(o.animateDistanceInPixels*3)+"px"}, o.animateVelocity).
+                    animate({"left": "-="+(o.animateDistanceInPixels*2)+"px"}, o.animateVelocity);
+          }
+        }
+
+        obj.css({'position': o.position});
+
+        if(o.loopInterval){
+          setInterval(function(){
+            animate.run(obj);
+          }, o.timeInterval);
+        } else {
+          setTimeout(function(){
+            animate.run(obj);
+          }, o.timeInterval);
+        }
+
+      });
+    }
+  });
+})(jQuery);
